d06b4c398ea2b2255f96f5278ed8f626996ef0d6
AkhminDenis <denis-akhmin@yandex.ru>
2025-11-16 20:45:45 +0300

    Task-6

---
+++ Task-3(6)/4/src/autoservice/ui/actions/general/CsvImportOrdersAction.java
@@ -0,0 +1,36 @@
+package autoservice.ui.actions.general;
+
+import autoservice.model.AutoService;
+import autoservice.model.exceptions.CsvParsingException;
+import autoservice.model.exceptions.IllegalGarageSpotSize;
+import autoservice.model.exceptions.ImportException;
+import autoservice.model.io.CsvImportService;
+import autoservice.ui.actions.IAction;
+
+import java.io.IOException;
+
+public class CsvImportOrdersAction implements IAction {
+    private final AutoService service;
+    public CsvImportOrdersAction(AutoService service) {
+        this.service = service;
+    }
+    @Override
+    public void execute() {
+        try {
+            if (!service.importOrders()){
+                System.out.println("Файл пуст.");
+            }
+            else{
+                System.out.println("Данные успешно импортированы!");
+            }
+        } catch(IllegalGarageSpotSize e){
+            System.out.println(e.getMessage());
+        } catch (ImportException e) {
+            System.out.println(e.getMessage());
+        } catch(IOException e){
+            System.out.println("Файл не найден. Создайте файл по пути <Task-3(6)/4/data/...>");
+        } catch(CsvParsingException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
